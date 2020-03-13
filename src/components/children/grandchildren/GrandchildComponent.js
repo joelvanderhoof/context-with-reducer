793404a2b31b7eb27b82b54d5e0c2b2122281da3
joelvanderhoof <joel.vanderhoof@toyota.com>
2020-03-13 12:42:13 -0700

    added a component level example of useReducer

--- src/components/children/grandchildren/GrandchildComponent.js
+++ src/components/children/grandchildren/GrandchildComponent.js
@@ -1,15 +1,35 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useReducer, useState } from 'react';
 import { Context } from 'Context';
 import GiphyTile from './GiphyTile'; 
 
+const localActions = {
+    setFoo: value => ({
+        type: 'FOO',
+        value,
+    })
+};
+
+const localReducer = (state = {}, action) => {
+    switch (action.type) {
+        case ('FOO'):
+            return {
+                ...state,
+                foo: action.value,
+            };
+        default:
+            return state;
+    }
+};
+
 const GrandchildComponent = () => {
     const [localThing, setLocalThing] = useState('');
+    const [localState, localDispatch] = useReducer(localReducer, {});
     const { actions, dispatch, state } = useContext(Context);
     const {firstName, lastName } = state;
 
     return (
         <div>
-            <h2>Name: {`${firstName} ${lastName} ${localThing}`}</h2>
+            <h2>Name: {`${firstName} ${lastName} ${localState.foo} ${localThing}`}</h2>
             <p>first name:</p>
             <input
                 onChange={event => dispatch(actions.setFirstName(event.target.value))}
@@ -20,7 +40,12 @@ const GrandchildComponent = () => {
                 onChange={event => dispatch(actions.setLastName(event.target.value))}
             />
 
-            <p>local thing:</p>
+            <p>local useReducer thing:</p>
+            <input
+                onChange={event => localDispatch(localActions.setFoo(event.target.value))}
+            />
+
+            <p>local useState thing:</p>
             <input
                 onChange={event => setLocalThing(event.target.value)}
             />
